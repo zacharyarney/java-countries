68e067910914fa1f7eacae6af3dcc70353088dea
zacharyarney <zachary.arney@gmail.com>
2019-12-11 15:54:44 -0800

    adds findCountries method in CountryList

--- src/main/java/com/lambdaschool/countries/CountryList.java
+++ src/main/java/com/lambdaschool/countries/CountryList.java
@@ -208,4 +208,14 @@ public class CountryList {
         countryList.add(new Country("Antigua and Barbuda", 104084, 440, 32));
         countryList.add(new Country("Seychelles", 95702, 460, 36));
     }
+
+    public ArrayList<Country> findCountries(Checker checker) {
+        ArrayList<Country> tempList = new ArrayList<>();
+        for (Country c : countryList) {
+            if (checker.check(c)) {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
 }
